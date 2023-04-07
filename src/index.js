98c2f2b4e30b833704be11c036f7ec3edda64f7e
Cherise Foster <cherise.foster@outlook.com>
2023-04-07 17:06:57 -0600

    start lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    renderMenu()
+
+})
+
+const menu = document.getElementById('ramen-menu')
+const image = document
+function renderMenu(){
+    fetch('http://localhost:3000/ramens')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+}
