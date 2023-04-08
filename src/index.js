74125540557bd8a74e12df8468b91e0614ab4182
Cherise Foster <cherise.foster@outlook.com>
2023-04-08 10:26:43 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-document.addEventListener('DOMContentLoaded', () => {
-    renderMenu()
+// document.addEventListener('DOMContentLoaded', () => {
+//     renderMenu()
 
-})
+// })
+const ramenAPI = 'http://localhost:3000/ramens';
+const ramenMenuDiv = el('ramen-menu');
 
-const menu = document.getElementById('ramen-menu')
-const image = document
-function renderMenu(){
-    fetch('http://localhost:3000/ramens')
+el('new-ramen').addEventListener('submit', newRamenHandler)
+
+// GET request
+fetch(ramenAPI)
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(renderRamens)
+
+// Renders each ramen to page
+function renderRamens(ramens){
+    ramens.forEach(renderOneRamen);
+}
+
+// Creates ramen img on page
+function renderOneRamen(ramen){
+    const ramenImageElement = document.createElement('img')
+    ramenImageElement.src = ramen.image
+    ramenMenuDiv.append(ramenImageElement);
+
+    // click event to bring ramen image clicked on to the center
+    ramenImageElement.addEventListener('click', () => ramenClickHandler(ramen))
+}
+
+// function that adds the ramen image to the center
+function ramenClickHandler(ramen){
+    el('detail-image').src = ramen.image;
+    el('detail-name').textContent = ramen.name;
+    el('detail-restaurant').textContent = ramen.restaurant
+    el('rating-display').textContent = ramen.rating
+    el('comment-display').textContent = ramen.comment
+}
+
+// gets elements by id in a function to shorten code
+function el(id){
+    return document.getElementById(id);
+}
+
+// creates new ramen after submitting form
+function newRamenHandler(e){
+    e.preventDefault();
+
+    const newRamen = {
+        name : e.target.name.value,
+        restaurant : e.target.restaurant.value,
+        image : e.target.image.value,
+        rating : e.target.rating.value,
+        comment : e.target['new-comment'].value
+    };
+    renderRamens(newRamen);;
+    e.target.reset()
 }
